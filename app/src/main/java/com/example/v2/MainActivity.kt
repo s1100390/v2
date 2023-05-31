0f1407d11e33fa0283350389daf2e4a28b6db6d2
s1100390 <s1100390@gm.pu.edu.tw>
2023-05-31 10:31:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/v2/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.v2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.v2.ui.theme.V2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            V2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "作者：資管二B 江祐丞")
+    Image(
+        painterResource(id = R.drawable.map),
+        contentDescription ="Cart button icon",
+        modifier = Modifier.size(20.dp))
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+    //V2Theme {
+        //Greeting("Android")
+    //}
+//}
